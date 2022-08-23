3f5ace06e8a13403cd32567b6a5331b1aef883dc
kelydev <kely.piscis.97@gmail.com>
2022-08-23 10:51:43 -0500

    Esta es mi cuarta prueba

--- src/Jpanel/Main.java
+++ src/Jpanel/Main.java
@@ -3,10 +3,8 @@ package Jpanel;
 public class Main {
    public static void main(String[] args)
    {
-       System.out.println("Hola Mundo");
-       System.out.println("Hola Mundo");
-       System.out.println("Hola Mundo");
-       System.out.println("Hola Mundo");
-       System.out.println("Hola Mundo");
+       System.out.println("Hola");
+       System.out.println("Mi nombre es kely");
+       System.out.println("Hola hola hola");
    }
 }
